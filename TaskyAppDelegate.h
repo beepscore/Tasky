c82401e9cfff61070d4ddbdcf52a0cd11c4bdf81
beepscore <support@beepscore.com>
2010-04-08 13:33:04 -0700

    Dalrymple Ch 14 uses NSTask to sort.  Use it as a template for this project.
    Ref: Dalrymple and Hillegass Advanced Mac OS X Programming Ch 14
    This commit version sorts correctly.
    TODO:  Change from sort to list using ls -la

--- TaskyAppDelegate.h
+++ TaskyAppDelegate.h
@@ -4,14 +4,22 @@
 //
 //  Created by Steve Baker on 4/7/10.
 //  Copyright 2010 Beepscore LLC. All rights reserved.
-//
+//  Ref: Dalrymple and Hillegass Advanced Mac OS X Programming Ch 14
 
 #import <Cocoa/Cocoa.h>
 
 @interface TaskyAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+    NSTextView *inText;
+    NSTextView *outText;
 }
 
+#pragma mark -
+#pragma mark properties
 @property (assign) IBOutlet NSWindow *window;
+@property(nonatomic,retain)IBOutlet NSTextView *inText;
+@property(nonatomic,retain)IBOutlet NSTextView *outText;
+
+- (IBAction) handleListButton:(id)sender;
 
 @end
--- TaskyAppDelegate.m
+++ TaskyAppDelegate.m
@@ -10,10 +10,55 @@
 
 @implementation TaskyAppDelegate
 
+#pragma mark -
+#pragma mark properties
 @synthesize window;
+@synthesize inText;
+@synthesize outText;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Insert code here to initialize your application 
 }
+
+- (IBAction) handleListButton:(id)sender {
+    
+    NSData *data;
+    NSPipe *inPipe, *outPipe;
+    NSFileHandle *writingHandle;
+    NSTask *task;
+    NSString *aString;
+    
+    task = [[NSTask alloc] init];
+    inPipe = [[NSPipe alloc] init];
+    outPipe = [[NSPipe alloc] init];
+    
+    [task setLaunchPath:@"/usr/bin/sort"];
+    [task setStandardInput:inPipe];
+    [task setStandardOutput:outPipe];
+    [task setArguments:[NSArray arrayWithObject:@"-f"]];
+    
+    [task launch];
+    
+    writingHandle = [inPipe fileHandleForWriting];
+    [writingHandle writeData:[[inText string] dataUsingEncoding:NSUTF8StringEncoding]];
+    [writingHandle closeFile];
+    
+    data = [[outPipe fileHandleForReading] readDataToEndOfFile];
+    aString = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+    [outText setString:aString];
+    [aString release];
+    [task release];
+    [inPipe release];
+    [outPipe release];
+}
+
+
+- (void)dealloc {
+    [inText release], inText = nil;
+    [outText release], outText = nil;
+    
+    [super dealloc];
+}
+
 
 @end
